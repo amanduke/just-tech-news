bda879fbc955e5ebfb9448b74ac33d9eeec896b4
amanduke <65379991+amanduke@users.noreply.github.com>
2020-11-08 08:57:37 -0500

    express

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
+// requires the style.css
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -17,6 +19,10 @@ db.connect({
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// the express.static() method is a built-in Express.js middleware function that
+// can take all of the contents of a folder and serve them as static assets
+// This is useful for front-end specific files like images, style sheets, and JavaScipt files
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
