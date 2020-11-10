f2735fa9837b288a4e2228ffd5b80aa299eacd60
amanduke <65379991+amanduke@users.noreply.github.com>
2020-11-09 21:41:43 -0500

    cookies session

---
+++ public/javascript/login.js
@@ -0,0 +1,32 @@
+const { response } = require("express");
+
+
+async function signupFormHandler(event) {
+    event.preventDefault();
+  
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+  
+    if (username && email && password) {
+      await fetch('/api/users', {
+        method: 'post',
+        body: JSON.stringify({
+          username,
+          email,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+      
+      // check the response status
+      if (response.ok) {
+          document.location.replace('/');
+      } else {
+          alert(response.statusText);
+      }
+    }
+  }
+  
+  document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+  
